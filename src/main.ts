5f48aa14ef367a34146087a082856c54e7d26d6e
Green Gem <chris_waitt@me.com>
2024-05-28 11:21:40 +0100

    chore: Align locale file name with ISO 639-1 standard and update validation message
    - Renamed jp.json to ja.json to conform to the ISO 639-1 standard for language codes.
    - Enhanced the validation message for the 'other service' field to specify the requirement of at least 2 characters.

--- src/main.ts
+++ src/main.ts
@@ -8,23 +8,33 @@ import App from './App.vue'
 import router from './router'
 import en from './locales/en.json'
 import es from './locales/es.json'
-import jp from './locales/jp.json'
+import ja from './locales/ja.json'
+import { OhVueIcon, addIcons } from 'oh-vue-icons'
+import { FaExternalLinkAlt } from 'oh-vue-icons/icons'
 
+// Configure i18n
 const i18n = createI18n({
-  legacy: false,
-  locale: 'en',
-  fallbackLocale: 'en',
+  legacy: false, // Setting legacy to false for Composition API support
+  locale: 'en', // Default locale
+  fallbackLocale: 'en', // Fallback locale
   messages: {
     en,
     es,
-    jp
+    ja
   }
 })
 
+// Add icons
+addIcons(FaExternalLinkAlt)
+
 const app = createApp(App)
 
+// Use plugins
 app.use(createPinia())
 app.use(i18n)
 app.use(router)
+
+// Register global components
+app.component('v-icon', OhVueIcon)
 
 app.mount('#app')
