5b48957316204e88d0eaf0066ea92aa9a0a2f470
Green Gem <chris_waitt@me.com>
2024-05-28 09:57:28 +0100

    feat: Add FormWrapper component for wrapping the form content

---
+++ src/components/form/FormWrapper.vue
@@ -0,0 +1,7 @@
+<template>
+  <div
+    class="bg-slate-900 py-5 md:py-7 px-5 md:px-10 rounded-lg shadow-xl max-w-screen-sm w-full items-center ring-8 ring-slate-500/10 backdrop-blur-md"
+  >
+    <slot></slot>
+  </div>
+</template>
