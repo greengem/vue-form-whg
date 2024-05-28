61cc2c52f3412e53f1aed99886b90a5e0460e90f
Green Gem <chris_waitt@me.com>
2024-05-28 10:01:41 +0100

    feat: Add form components and update HomeView
    - Add FormWrapper, FormHeader, FormComponent, and LanguageSwitcher components to the HomeView
    - Update the template to use the new form components for a structured form layout

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,14 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import FormWrapper from '@/components/form/FormWrapper.vue'
+import FormHeader from '@/components/form/FormHeader.vue'
+import FormComponent from '@/components/form/FormComponent.vue'
+import LanguageSwitcher from '@/components/LanguageSwitcher.vue'
+</script>
 
 <template>
-  <main>Hello World!</main>
+  <FormWrapper>
+    <FormHeader />
+    <LanguageSwitcher />
+    <FormComponent />
+  </FormWrapper>
 </template>
