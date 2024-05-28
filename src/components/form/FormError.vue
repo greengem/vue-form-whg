43a9741339d45891d476426b7d5a103c2c6ee279
Green Gem <chris_waitt@me.com>
2024-05-28 09:57:04 +0100

    feat: Add FormError component for displaying form validation errors

---
+++ src/components/form/FormError.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="text-red-600 text-sm h-5 mt-1">
+    <span v-if="error">{{ error }}</span>
+  </div>
+</template>
+
+<script setup lang="ts">
+const { error } = defineProps({
+  error: String
+})
+</script>
