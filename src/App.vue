a849351dbd7e5c06157082cc7f984741f3330a36
Green Gem <chris_waitt@me.com>
2024-05-28 08:58:01 +0100

    feat: Add unstyled LanguageSwitcher component for language selection

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import LanguageSwitcher from '@/components/LanguageSwitcher.vue'
+</script>
 
 <template>
+  <LanguageSwitcher />
   <RouterView />
 </template>
