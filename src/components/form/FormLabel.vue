81bc84a44edac5c6130d95676f4c8ca7cee0c25d
Green Gem <chris_waitt@me.com>
2024-05-28 09:57:33 +0100

    feat: Add FormLabel component for displaying form field labels

---
+++ src/components/form/FormLabel.vue
@@ -0,0 +1,11 @@
+<template>
+  <label :for="forField" class="block text-sm font-medium text-slate-200 mb-1">
+    <slot></slot>
+  </label>
+</template>
+
+<script setup lang="ts">
+const { forField } = defineProps({
+  forField: String
+})
+</script>
