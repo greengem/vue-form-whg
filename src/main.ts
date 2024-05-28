5ca8f8307de6872aa1b09827db3864c623848647
Green Gem <chris_waitt@me.com>
2024-05-28 08:53:46 +0100

    Configured vue-i18n and added example translations. - Translations include English, Spanish, and Japanese. - Error messages included in translations.

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,29 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createI18n } from 'vue-i18n'
 
 import App from './App.vue'
 import router from './router'
+import en from './locales/en.json'
+import es from './locales/es.json'
+import jp from './locales/jp.json'
+
+const i18n = createI18n({
+  legacy: false,
+  locale: 'en',
+  fallbackLocale: 'en',
+  messages: {
+    en,
+    es,
+    jp
+  }
+})
 
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(i18n)
 app.use(router)
 
 app.mount('#app')
