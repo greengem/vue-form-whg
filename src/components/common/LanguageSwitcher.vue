cc139fcccea32cd4b38bf1c8ec2d318ce6295214
Green Gem <chris_waitt@me.com>
2024-05-29 11:22:51 +0100

    Stlying for LanguageSwitcher component. - Added tailwind stlying - Use of `appearance-none` to remove the gloss effect in webkit browsers. - Use of an absolute element to apply an svg chevron icon to the right of the select element.

--- src/components/common/LanguageSwitcher.vue
+++ src/components/common/LanguageSwitcher.vue
@@ -1,14 +1,25 @@
 <template>
-  <div class="locale-changer">
-    <select v-model="selectedLocale" @change="changeLocale">
+  <div class="locale-changer relative">
+    <select
+      v-model="selectedLocale"
+      @change="changeLocale"
+      class="text-sm w-14 px-3 py-2 rounded-lg bg-slate-200 dark:bg-slate-800 dark:text-slate-100 shadow-md ring-1 ring-slate-300 dark:ring-slate-700 appearance-none"
+    >
       <option v-for="locale in availableLocales" :key="`locale-${locale}`" :value="locale">
         {{ locale }}
       </option>
     </select>
+    <div
+      class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-3 text-gray-700"
+    >
+      <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+        <path d="M7 10l5 5 5-5H7z" />
+      </svg>
+    </div>
   </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
 import { computed } from 'vue'
 import { useI18n } from 'vue-i18n'
 
