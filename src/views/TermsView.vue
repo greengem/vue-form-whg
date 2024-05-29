74e428f27d3e2acb7cc07f9d5b2fbbacf057898c
Green Gem <chris_waitt@me.com>
2024-05-29 11:25:35 +0100

    Styled Terms page and added dummy text

--- src/views/TermsView.vue
+++ src/views/TermsView.vue
@@ -1,3 +1,14 @@
+<script setup lang="ts">
+import FormWrapper from '@/components/form/FormWrapper.vue'
+import FormHeader from '@/components/form/FormHeader.vue'
+import { useI18n } from 'vue-i18n'
+
+const { t } = useI18n()
+</script>
+
 <template>
-  <h1>Terms Page</h1>
+  <FormWrapper>
+    <FormHeader>{{ t('terms.title') }}</FormHeader>
+    <p class="mb-5">{{ t('terms.welcomeMessage') }}</p>
+  </FormWrapper>
 </template>
