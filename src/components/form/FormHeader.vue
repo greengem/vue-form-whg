5453a4ebac818d54af1607f444795db94a7d996c
Green Gem <chris_waitt@me.com>
2024-05-29 15:27:41 +0100

    Update FormHeader and FormWrapper components for responsive design

--- src/components/form/FormHeader.vue
+++ src/components/form/FormHeader.vue
@@ -3,8 +3,8 @@ import LanguageSwitcher from '@/components/common/LanguageSwitcher.vue'
 </script>
 
 <template>
-  <header class="flex items-center justify-between mb-7">
-    <h1 class="text-2xl md:text-3xl tracking-tight">
+  <header class="flex items-center justify-between mb-5 md:mb-10">
+    <h1 class="text-3xl md:text-4xl tracking-tight">
       <slot></slot>
     </h1>
     <LanguageSwitcher />
