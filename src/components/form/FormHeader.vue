c1d691a67ca03c11746fa10d8979b7fe27d0f370
Green Gem <chris_waitt@me.com>
2024-05-29 11:26:03 +0100

    Modified FormHeader to accept title as a slot so it can be reused

--- src/components/form/FormHeader.vue
+++ src/components/form/FormHeader.vue
@@ -5,7 +5,7 @@ import LanguageSwitcher from '@/components/common/LanguageSwitcher.vue'
 <template>
   <header class="flex items-center justify-between mb-7">
     <h1 class="text-2xl md:text-3xl dark:text-slate-50 tracking-tight">
-      {{ $t('form.formTitle') }}
+      <slot></slot>
     </h1>
     <LanguageSwitcher />
   </header>
